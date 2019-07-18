92c93181e8f87a263b466dd0fef1a092237750bc
Salvador Hernández Salvatierra <astrargalar@gmail.com>
2019-07-18 19:33:22 +0200

    Agregada portada a front-page.php y sus estilos

--- front-page.php
+++ front-page.php
@@ -1,5 +1,53 @@
 <?php
 get_header();
-get_template_part('template-parts/main-loop');
+get_template_part('template-parts/main-loop'); ?>
+<div class="open">
+    <div class="layer"></div>
+    <div class="layer"></div>
+</div>
+<section>
+    <div class="bannerText">
+        <h2>HAZLO CON WORDPRESS</h2><br>
+        <h3>Sitios web que venden</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a href="#">Leer más</a>
+    </div>
+    <img src="http://localhost/tswp/wp-content/uploads/2019/07/bulb-on.png" class="bulb">
+    <ul class="sci">
+        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+    </ul>
+    <div class="element1"></div>
+    <div class="element2"></div>
+    <div class="scrolldown">
+        <a href="http://127.0.0.1:5500/">
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+    </div>
+</section>
+/*-- circulo que gira --*/
+<div id="padre">
+    <div id="hijo">
+        <div id="hijo">
+            <div id="hijo">
+                <div id="hijo">
+                    <div id="hijo">
+                        <div id="hijo"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
 get_sidebar();
 get_footer();
